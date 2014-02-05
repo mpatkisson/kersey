03de3140ce3aa8c8a987ab979ba8d7fe433a979d
Mike Atkisson <mpatkisson@gmail.com>
2014-02-05 03:18:39 -0500

    Began unit testing and refactoring of the batch inserter.

---
+++ test/inserter.js
@@ -0,0 +1,40 @@
+/*global describe,it */
+
+var assert = require('assert'),
+    inserter = require('../lib/inserter');
+
+describe('inserter', function () {
+    'use strict';
+
+    var tested = inserter.create({
+        database: 'kersey',
+        collection: 'inserter.test',
+        threshold: 2
+    });
+
+    describe('#open()', function () {
+        it('should return an open MongoDB client', function (done) {
+            tested.open(function opened(client) {
+                assert.notEqual(client, null);
+                done();
+            });
+        });
+    });
+
+    describe('#insert()', function () {
+        it('should not insert records before batch threshold is met', function (done) {
+            var record = {a: 1};
+
+            function queued(err, queueSize, insertedSize) {
+                if (err) {
+                    throw err;
+                }
+                assert.equal(queueSize, 1);
+                assert.equal(insertedSize, 0);
+                done();
+            }
+
+            tested.insert(record, false, queued);
+        });
+    });
+});
