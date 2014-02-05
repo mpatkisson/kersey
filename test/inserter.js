c96085fbd2c700a38b3c5c91990e90ccee8884f6
Mike Atkisson <mpatkisson@gmail.com>
2014-02-05 04:10:49 -0500

    Added more tests for the batch inserter.
    Refactored the callback on the inserter's queue method and removed
    the test.js vanilla Mocha test file used to ensure Mocha was working.

--- test/inserter.js
+++ test/inserter.js
@@ -1,15 +1,42 @@
-/*global describe,it */
+/*global describe,beforeEach,it */
 
 var assert = require('assert'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
     inserter = require('../lib/inserter');
 
 describe('inserter', function () {
     'use strict';
 
-    var tested = inserter.create({
-        database: 'kersey',
-        collection: 'inserter.test',
-        threshold: 2
+    var host = 'localhost',
+        port = 27017,
+        database = 'kersey',
+        collection = 'inserter.test',
+        tested = inserter.create({
+            host: host,
+            port: port,
+            database: database,
+            collection: collection,
+            threshold: 2
+        });
+
+    afterEach(function (done) {
+        var server = new Server(host, port),
+            client = new MongoClient(server);
+        client.open(function (err, opened) {
+            if (err) {
+                throw err;
+            }
+            var db = opened.db(database);
+            db.collection(collection).remove(null, {w: 1}, function (err, removed) {
+                if (err) {
+                    throw err;
+                }
+                assert.ok(removed >= 0);
+                done();
+            });
+        });
+
     });
 
     describe('#open()', function () {
@@ -25,16 +52,43 @@ describe('inserter', function () {
         it('should not insert records before batch threshold is met', function (done) {
             var record = {a: 1};
 
-            function queued(err, queueSize, insertedSize) {
+            function queued(err, queueSize) {
                 if (err) {
                     throw err;
                 }
                 assert.equal(queueSize, 1);
-                assert.equal(insertedSize, 0);
                 done();
             }
 
             tested.queue(record, false, queued);
+        });
+
+        it('should insert records when the batch threshold is met', function (done) {
+            var record = {a: 2};
+
+            function queued(err, queueSize) {
+                if (err) {
+                    throw err;
+                }
+                assert.equal(queueSize, 0);
+                done();
+            }
+
+            tested.queue(record, false, queued);
+        });
+
+        it('should insert records when isLast is enabled', function (done) {
+            var record = {a: 3};
+
+            function queued(err, queueSize) {
+                if (err) {
+                    throw err;
+                }
+                assert.equal(queueSize, 0);
+                done();
+            }
+
+            tested.queue(record, true, queued);
         });
     });
 });
